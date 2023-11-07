94e93ae6f8218027fb7b47c9e4550eee3e8fe35f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-11-07 15:16:54 -0800

    Oops, scipy is now actually installed during testing.

--- tests/test_package_utils.py
+++ tests/test_package_utils.py
@@ -4,15 +4,7 @@ __author__ = "jkanche"
 __copyright__ = "jkanche"
 __license__ = "MIT"
 
-
-
-def test_for_scipy():
-    pkg = is_package_installed("scipy")
-
-    assert pkg is False
-
-
-def test_for_numpy():
-    pkg = is_package_installed("numpy")
-
-    assert pkg is True
+def test_installed_package():
+    assert is_package_installed("scipy")
+    assert is_package_installed("numpy")
+    assert not is_package_installed("some_random_package")

42964d7b258f03fa7ea2e6228cc1b05630bf52bf
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2025-01-14 18:07:41 -0800

    Fix is list on Numpy's masked arrays

--- tests/test_list_type_checks.py
+++ tests/test_list_type_checks.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.ma as ma
 import pytest
 from biocutils import is_list_of_type
 
@@ -29,3 +30,13 @@ def test_numpy_elems():
     x = [np.random.rand(3), np.random.rand(3, 2)]
 
     assert is_list_of_type(x, np.ndarray)
+
+def test_numpy_arrays():
+    x = np.random.rand(3)
+
+    assert is_list_of_type(x, float)
+
+    x = ma.array([1, None, 3], mask = [0, 1, 0], dtype=np.float32)
+
+    assert is_list_of_type(x, np.float32, ignore_none=True)
+    assert is_list_of_type(x, np.float32) is False

e4d9f8bb4b89e709ea62daa119ceddd6b395334f
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2025-01-15 08:01:21 -0800

    Fix is list on Numpy's masked arrays (#25)

--- src/biocutils/is_list_of_type.py
+++ src/biocutils/is_list_of_type.py
@@ -1,13 +1,14 @@
 from typing import Callable, Union
 
+import numpy as np
+import numpy.ma as ma
+
 __author__ = "jkanche"
 __copyright__ = "jkanche"
 __license__ = "MIT"
 
 
-def is_list_of_type(
-    x: Union[list, tuple], target_type: Callable, ignore_none: bool = False
-) -> bool:
+def is_list_of_type(x: Union[list, tuple], target_type: Callable, ignore_none: bool = False) -> bool:
     """Checks if ``x`` is a list, and whether all elements of the list are of the same type.
 
     Args:
@@ -19,10 +20,16 @@ def is_list_of_type(
 
     Returns:
         True if ``x`` is a list or tuple and all elements are of the target
-        type (or None, if ``ignore_none = True``). Otherwise, false.
+        type (or None, if ``ignore_none = True``). Otherwise, False.
     """
-    if not isinstance(x, (list, tuple)):
+    if not isinstance(x, (list, tuple, np.ndarray, ma.MaskedArray)):
         return False
+    
+    if isinstance(x, ma.MaskedArray):
+        if not ignore_none:
+            return all(x.mask) and all(isinstance(item, target_type) for item in x.data)
+        else:
+            return all(isinstance(item, target_type) for item in x.data[x.mask])
 
     if not ignore_none:
         return all(isinstance(item, target_type) for item in x)
